a314e67daa84f167a7b042d50dfd35ae13ab98f5
silica67sdm <1127lse@gmail.com>
2025-08-12 16:28:33 +0900

    create blockchain class

--- blockchain.js
+++ blockchain.js
@@ -23,8 +23,50 @@ class Block {
 	}
 }
 
-const block1 = new Block(['Transaction 1']);
-console.log(block1);
+class Blockchain {
+	constructor() {
+		this.chain = [new Block(['Genesis Transaction'])];
+		this.difficulty = 2;
+		this.blockTime = 5000;
+	}
 
-block1.mine(5);
-console.log(block1);
+	getLastBlock() {
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(block) {
+		block.prevHash = this.getLastBlock().hash;
+		block.mine(this.difficulty);
+
+		this.chain.push(block);
+		this.difficulty += Date.now() - this.getLastBlock().timestamp < this.blockTime ? 1 : -1;
+	}
+
+	isValid() {
+		for (let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const prevBlock = this.chain[i - 1];
+
+			if (currentBlock.hash !== currentBlock.getHash() || currentBlock.prevHash !== prevBlock.hash) {
+				return false;
+			}
+			return true;
+		}
+	}
+}
+
+const MyBlockChain = new Blockchain();
+MyBlockChain.addBlock(new Block(['Transaction 1']));
+MyBlockChain.addBlock(new Block(['Transaction 2']));
+MyBlockChain.addBlock(new Block(['Transaction 3']));
+
+
+console.log(MyBlockChain.chain);
+console.log(`MyBlockChain Valid Check: ${MyBlockChain.isValid()}`);
+
+
+// const block1 = new Block(['Transaction 1']);
+// console.log(block1);
+
+// block1.mine(5);
+// console.log(block1);
